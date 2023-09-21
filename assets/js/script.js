4ce3af7a2efb2d3f92cba56bb38be8079a0e93b5
Gsandman8 <gage.e.sandman@gmail.com>
2023-09-21 14:12:53 -0700

    Added posters for each movie

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ searchForm.addEventListener('submit', function(e){
 
 const genreBtn = document.getElementById("genreBtn");
 const genreList = document.getElementById("genreList");
-const div = document.getElementById("movieList");
+const movieList = document.getElementById("movieList");
 const genreIdList = {
     action: 28,
     adventure: 12,
@@ -89,7 +89,7 @@ genreBtn.addEventListener("click", function(event){
     genre = genreList.value;
     genreId = genreIdList[genre];
     console.log(genreId);
-    div.textContent="";
+    movieList.textContent="";
     getMovieApi(genreId);
 })
 
@@ -102,12 +102,17 @@ function getMovieApi(genreId){
     .then(function (data) {
     console.log(data);
     for(let i=0; i<data.results.length; i++){
-        let header = document.createElement("h1");
-        let p = document.createElement("p");
-        header.textContent = data.results[i].original_title
-        p.textContent = data.results[i].overview
-        div.appendChild(header);
-        div.appendChild(p);
+        let title = document.createElement("h1");
+        let movie = document.createElement("p");
+        let poster = document.createElement("img");
+        let poster_path = "https://image.tmdb.org/t/p/original"+ data.results[i].poster_path;
+        title.textContent = data.results[i].original_title
+        movie.textContent = data.results[i].overview
+        poster.setAttribute("src", poster_path)
+        poster.setAttribute("class", "poster");
+        movieList.appendChild(title);
+        movieList.appendChild(poster);
+        movieList.appendChild(movie);
 
         
     }
